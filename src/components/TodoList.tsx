f98e857d5d380853a25d38a47773d96ced5876d7
Alexdev111 <gurgurov.alexandru001@gmail.com>
2025-07-06 18:32:02 +0300

    First commit TodoList

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,106 @@
+import { useState } from "react";
+
+const TodoList = () => {
+  const [tasks, setTask] = useState([
+    "Go to work",
+    "Drain the pool",
+    "Eat a pizza",
+    "Buy a new car",
+  ]);
+  const [newTask, setNewTask] = useState("");
+  const [category, setCategory] = useState("");
+
+  function handleInputChangeTask(event) {
+    setNewTask(event.target.value);
+  }
+
+  function handleInputChangeCategory(event) {
+    setCategory(event.target.value);
+  }
+  function addTask() {
+    if (newTask.trim() !== "") {
+      setTask((t) => [...t, newTask]);
+      console.log([...tasks, newTask]);
+      setCategory("");
+      setNewTask("");
+    }
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (e.target.category.value.length >= 1) {
+      console.log(e.target.category.value);
+      setCategory("Category:" + e.target.category.value);
+    } else {
+      console.error("Category must be a single character");
+    }
+    if (e.target.task.value.length >= 1) {
+      console.log(e.target.task.value);
+      setCategory(e.target.category.value);
+    } else {
+      console.error("Task must be a single character");
+    }
+  }
+  return (
+    <div>
+      <h2>Project Progress: 0%</h2>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <div className="row">
+            <input
+              type="text"
+              name="task"
+              value={newTask}
+              onChange={handleInputChangeTask}
+              placeholder="New Task Title"
+              className="form-control"
+              style={{ width: "auto" }}
+            />
+            <input
+              type="text"
+              name="category"
+              value={category}
+              onChange={handleInputChangeCategory}
+              placeholder="Category"
+              className="form-control"
+              style={{ width: "auto" }}
+            />
+            <button
+              className="btn btn-primary"
+              type="submit"
+              style={{ width: "auto", marginRight: "5px" }}
+              onClick={addTask}
+            >
+              Add Task
+            </button>
+            <input
+              type="text"
+              placeholder="Task ID to remove"
+              className="form-control"
+              style={{ width: "auto" }}
+            />
+            <button
+              className="btn btn-primary"
+              type="submit"
+              style={{ width: "auto" }}
+            >
+              Remove Task
+            </button>
+          </div>
+        </form>
+      </div>
+      <div className="task-element">
+        <h2>{category + " " + `(0% completed, ${tasks.length} tasks)`} </h2>
+        <ol className="list-group">
+          {tasks.map((task, index) => (
+            <li key={index}>
+              <ul className="list-group-item">{task}</ul>
+            </li>
+          ))}
+        </ol>
+      </div>
+    </div>
+  );
+};
+
+export default TodoList;
